a7162ea6a7bf5a14f14c442bebb7d6f2ceec9d98
masterarrow <masterarrows@gmail.com>
2020-04-20 10:49:47 +0300

    Secure data

--- src/config/firebaseConfig.js
+++ src/config/firebaseConfig.js
@@ -5,14 +5,14 @@ import 'firebase/auth'
 
 // Firebase configuration
 const config = {
-  apiKey: "AIzaSyAj3vRd3wYP8rgpJUeWFqYBdO8cVmsjIWU",
-  authDomain: "movie-dbase.firebaseapp.com",
-  databaseURL: "https://movie-dbase.firebaseio.com",
-  projectId: "movie-dbase",
-  storageBucket: "movie-dbase.appspot.com",
-  messagingSenderId: "623065281210",
-  appId: "1:623065281210:web:24a36714b099cb5a5c8883",
-  measurementId: "G-7N4JHFVM8H"
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  databaseURL: process.env.DATABASE_URL,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGING_SENDER_ID,
+  appId: process.env.APP_ID,
+  measurementId: process.env.MEASUREMENT_ID
 };
 
 firebase.initializeApp(config);
